8969232cc429727f3438b9a8c1b7ba417f3b18b7
ARIEL OFIR CHOCRON <49628292@ort>
2026-04-16 09:03:05 -0300

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP_01_Chocron_Schatzyki_Ducatenzeiler.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
